2e95040326497554a87c2b5132700fb30649c9af
Bigggers <986794908@qq.com>
2019-09-11 18:18:49 +0800

    Initial commit

---
+++ app/src/main/java/com/example/jump/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.jump;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity{
+    private Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn=findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it=new Intent();
+                it.setClass(MainActivity.this,jump.class);
+                MainActivity.this.startActivity(it);
+            }
+        });
+    }
+
+}
